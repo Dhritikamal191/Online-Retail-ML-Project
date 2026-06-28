87898bb6e64079aab101d7dd72fc4604826813a3
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-28 17:38:56 +0530

    Create preprocessing.py

---
+++ .streamlit/preprocessing.py
@@ -0,0 +1,111 @@
+import os
+import pandas as pd
+
+from src.logger import logger
+from src.config import load_config
+
+config = load_config()
+
+
+class DataPreprocessor:
+
+    def __init__(self, df):
+
+        self.df = df.copy()
+
+    def remove_missing_customer(self):
+
+        logger.info("Removing Missing CustomerID")
+
+        before = len(self.df)
+
+        self.df = self.df.dropna(subset=["CustomerID"])
+
+        after = len(self.df)
+
+        logger.info(f"Removed {before-after} rows")
+
+    def remove_cancelled_orders(self):
+
+        logger.info("Removing Cancelled Orders")
+
+        before = len(self.df)
+
+        self.df = self.df[
+            ~self.df["InvoiceNo"].astype(str).str.startswith("C")
+        ]
+
+        after = len(self.df)
+
+        logger.info(f"Removed {before-after} cancelled invoices")
+
+    def remove_invalid_quantity(self):
+
+        logger.info("Removing Invalid Quantity")
+
+        before = len(self.df)
+
+        self.df = self.df[self.df["Quantity"] > 0]
+
+        after = len(self.df)
+
+        logger.info(f"Removed {before-after} rows")
+
+    def remove_invalid_price(self):
+
+        logger.info("Removing Invalid UnitPrice")
+
+        before = len(self.df)
+
+        self.df = self.df[self.df["UnitPrice"] > 0]
+
+        after = len(self.df)
+
+        logger.info(f"Removed {before-after} rows")
+
+    def convert_date(self):
+
+        logger.info("Converting InvoiceDate")
+
+        self.df["InvoiceDate"] = pd.to_datetime(
+            self.df["InvoiceDate"]
+        )
+
+    def create_total_price(self):
+
+        logger.info("Creating TotalPrice Feature")
+
+        self.df["TotalPrice"] = (
+            self.df["Quantity"] *
+            self.df["UnitPrice"]
+        )
+
+    def save(self):
+
+        path = config["data"]["processed_data"]
+
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        self.df.to_csv(path, index=False)
+
+        logger.info(f"Processed Dataset Saved : {path}")
+
+    def preprocess(self):
+
+        self.remove_missing_customer()
+
+        self.remove_cancelled_orders()
+
+        self.remove_invalid_quantity()
+
+        self.remove_invalid_price()
+
+        self.convert_date()
+
+        self.create_total_price()
+
+        self.save()
+
+        logger.info("Preprocessing Completed Successfully")
+
+        return self.df

5741b7ba9230ec03b59546831ed0072c34a23a01
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-28 17:36:57 +0530

    Update run_validation.py

--- .streamlit/run_validation.py
+++ .streamlit/run_validation.py
@@ -1,3 +1,5 @@
+import os
+
 from src.data_ingestion import DataIngestion
 from src.data_validation import DataValidation
 
@@ -8,5 +10,16 @@ df = ingestion.load_data()
 validator = DataValidation(df)
 
 validator.validate()
+
+os.makedirs("reports", exist_ok=True)
+
+missing.to_csv("reports/missing_values.csv")
+
+duplicate_rows = self.df[self.df.duplicated()]
+
+duplicate_rows.to_csv(
+    "reports/duplicate_rows.csv",
+    index=False
+)
 
 print("Validation Completed Successfully")

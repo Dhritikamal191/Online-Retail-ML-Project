df6884af55b2979a0f3a6c64f4fdec7368131107
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-07-04 23:18:29 +0530

    Update prediction.py

--- dashboard/prediction.py
+++ dashboard/prediction.py
@@ -24,7 +24,7 @@ def prediction_page():
     # LOAD DATA
     # ==========================================
 
-    df = load_scaled_dataset()
+    scaled_df = load_scaled_dataset()
 
     # ==========================================
     # CUSTOMER INPUT

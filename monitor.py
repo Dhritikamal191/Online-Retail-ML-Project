d9bd341615165131f01dec7023ded1cf084ebbd8
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-07-07 08:21:00 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,12 +6,12 @@ LOG_FILE = "artifacts/logs/prediction_logs.csv"
 def get_monitoring_metrics():
 
     if not os.path.exists(LOG_FILE):
-       return {"Total Predictions": 0, "Latest Prediction": None, "Cluster Distribution": {}}
- 
-    df = pd.read_csv(LOG_FILE, engine= "python",on_bad_lines="skip")
+       return {"Total Predictions": 0, "Latest Prediction": None, "Cluster Distribution": {}} 
+    
+       df = pd.read_csv(LOG_FILE, engine= "python",on_bad_lines="skip")
 
-    metrics = {"Total Predictions": len(df), "Latest Prediction": df.iloc[-1].to_dict(), "Cluster Distribution": df["PredictedCluster"].value_counts().to_dict()}
+       metrics = {"Total Predictions": len(df), "Latest Prediction": df.iloc[-1].to_dict(), "Cluster Distribution": df["PredictedCluster"].value_counts().to_dict()}
 
-    return metrics
+return metrics
   
 print(get_monitoring_metrics())

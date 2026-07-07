552c94e95caf7e074de70cc25facde14b12430f5
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-07-07 08:12:40 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,7 +8,7 @@ def get_monitoring_metrics():
     if not os.path.exists(LOG_FILE):
        return {"Total Predictions": 0, "Latest Prediction": None, "Cluster Distribution": {}}
  
-    df = pd.read_csv(LOG_FILE)
+    df = pd.read_csv(LOG_FILE, engine= "python",on_bad_lines="skip")
 
     metrics = {"Total Predictions": len(df), "Latest Prediction": df.iloc[-1].to_dict(), "Cluster Distribution": df["PredictedCluster"].value_counts().to_dict()}
 
